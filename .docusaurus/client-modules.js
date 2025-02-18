2d5e9e4e8b2f052bd48415c6a2ecbfa97999056f
Julius Cinco Cesar <knznsmn@icloud.com>
2025-02-18 09:15:25 +0400

    Add draft for logo

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("/home/knznsmn/Depositaire/knznsmn/mjajc/node_modules/infima/dist/css/default/default.css"),
-  require("/home/knznsmn/Depositaire/knznsmn/mjajc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/knznsmn/Depositaire/knznsmn/mjajc/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/knznsmn/Depositaire/knznsmn/mjajc/src/css/custom.css"),
+  require("/Volumes/Shitcake/_/Depositaire/knznsmn/mjajc/node_modules/infima/dist/css/default/default.css"),
+  require("/Volumes/Shitcake/_/Depositaire/knznsmn/mjajc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Volumes/Shitcake/_/Depositaire/knznsmn/mjajc/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Volumes/Shitcake/_/Depositaire/knznsmn/mjajc/src/css/custom.css"),
 ];
