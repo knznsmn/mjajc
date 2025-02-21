0b2e95096a6f6aded609f21b7dcf38c5443f8d58
Julius Cinco Cesar <knznsmn@icloud.com>
2025-02-21 15:59:28 +0400

    Add npm files to .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Volumes/Shitcake/_/Depositaire/knznsmn/mjajc/node_modules/infima/dist/css/default/default.css"),
-  require("/Volumes/Shitcake/_/Depositaire/knznsmn/mjajc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Volumes/Shitcake/_/Depositaire/knznsmn/mjajc/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Volumes/Shitcake/_/Depositaire/knznsmn/mjajc/src/css/custom.css"),
-];
