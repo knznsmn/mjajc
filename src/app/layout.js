e362e70f037bb4d734b90b2f2ae214e97cd333f5
Julius Cinco Cesar <knznsmn@knznsmn.com>
2025-04-25 11:00:53 +0400

    Add favicons

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="apple-mobile-web-app-title" content="MJAJC" />
       <body>
         {children}
       </body>
